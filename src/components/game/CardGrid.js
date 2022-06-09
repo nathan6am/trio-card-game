fb6a3b72de7980b3ef79e24acc194bc3128e3fb5
nathan6am <nathanpho47@gmail.com>
2022-06-09 10:23:14 -0400

    Chrome styling fixes

--- src/components/game/CardGrid.js
+++ src/components/game/CardGrid.js
@@ -13,7 +13,7 @@ export default function CardGrid({
       <div className="px-5 md:max-w-[80vw]">
         {timeLimit && <TimerProgressBar percent={percentTimeRemaining} />}
       </div>
-      <div className="grid grid-cols-3 p-2 overflow-hidden sm:grid-cols-4 md:gap-5 sm:p-5 min-w-[65vw] md:max-w-[80vw] lg:gap-6">
+      <div className="grid grid-cols-3 p-2 overflow-hidden sm:grid-cols-4 md:gap-5 sm:p-5 w-full min-w-[65vw] md:max-w-[80vw] lg:gap-6">
         {cards.map((card, idx) => {
           return (
             <Card
